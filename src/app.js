44039f563e0eca751c36d7cb8af3318bba8f9243
jonatas57 <jdjonatasduarte57@gmail.com>
2021-06-07 18:52:42 -0300

    test mongodb

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 const fs = require('fs')
 const express = require('express')
 const { getFilename } = require('./util')
+const { MongoClient } = require("mongodb")
+const uri = process.env.MONGODB_URI
+
+const client = new MongoClient(uri);
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
 
 app.post('/log', async (req, res) => {
-    fs.writeFile(getFilename(), JSON.stringify(req.body), (err) => {
-        if (err) {
-            res.status('500').send({error: err});
-        } else {
-            res.status(200).send()
-        }
-        
-    })
+    try {
+        await client.connect();
+        const db = client.db("kaluluDB");
+
+        const col = db.collection("datafiles");
+
+        let file = req.body;
+        const p = await col.insertOne(file);
+    } 
+    catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
 })
 
 app.get('/', (req, res) => res.send("<html>\n<body>\nTeste\n</body>\n</html>"))
