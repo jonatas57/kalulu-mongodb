98ee91a09390515b33e1b08d973ceae5e6a74200
jonatas57 <jdjonatasduarte57@gmail.com>
2021-06-07 17:10:21 -0300

    fix index

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.post('/log', async (req, res) => {
     })
 })
 
-app.get('/', (req, res) => res.render("index"))
+app.get('/', (req, res) => res.render("index.html"))
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is up on port 3000...')
