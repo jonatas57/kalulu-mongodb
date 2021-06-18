1cfb5778e27eccef86a6547af1a43381385db1cc
jonatas57 <jdjonatasduarte57@gmail.com>
2021-06-18 10:40:13 -0300

    update post

--- src/app.js
+++ src/app.js
@@ -10,23 +10,22 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 
 app.post('/log', async (req, res) => {
-  console.log("Post OK");
-  res.status(200).send();
-//    try {
-//        await client.connect();
-//        const db = client.db("kaluluDB");
-//
-//        const col = db.collection("datafiles");
-//
-//        let file = req.body;
-//        const p = await col.insertOne(file);
-//    } 
-//    catch (err) {
-//        console.log(err.stack);
-//    }
-//    finally {
-//        await client.close();
-//    }
+    try {
+        await client.connect();
+        const db = client.db("kaluluDB");
+
+        const col = db.collection("datafiles");
+
+        let file = req.body;
+        const p = await col.insertOne(file);
+        res.status(200).send();
+    } 
+    catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
 })
 
 app.get('/', (req, res) => res.send("<html>\n<body>\nTeste\n</body>\n</html>"))
