279dfb1dafc4421f6667be17b18e690e67b66754
jonatas57 <jdjonatasduarte57@gmail.com>
2021-06-18 11:25:20 -0300

    update limit 7

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri);
 
 const app = express()
-app.use(express.urlencoded({ limit: 102400, extended: true }))
-app.use(express.json({ limit: 102400, extended: true }))
+app.use(express.urlencoded({ limit: 102400, extended: true, parameterLimit: 100000 }))
+app.use(express.json({ limit: 102400, extended: true, parameterLimit: 100000 }))
 
 app.post('/log', async (req, res) => {
   console.log("Post OK");
